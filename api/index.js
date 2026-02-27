c1fe480ed09ae6c7cb73fbc87293daad5aa6ea1d
René Valencia Vallejo <renevalencia@gmail.com>
2026-02-27 16:22:17 -0500

    fix: configure Vercel for Angular SSR serverless deployment

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
-import { reqHandler } from '../dist/mvp/server/server.mjs';
-
-export default reqHandler;
+export default async (req, res) => {
+  const { reqHandler } = await import('../dist/mvp/server/server.mjs');
+  return reqHandler(req, res);
+};
