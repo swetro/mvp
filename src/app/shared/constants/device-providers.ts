a7d8bd43e4267b488301b39cf27bfaa68f43f38b
René Valencia Vallejo <renevalencia@gmail.com>
2026-03-28 19:41:08 -0500

    refactor(devices): move device brand images to images/device/ subfolder

--- src/app/shared/constants/device-providers.ts
+++ src/app/shared/constants/device-providers.ts
@@ -5,31 +5,31 @@ export const DEVICE_PROVIDERS: DeviceProvider[] = [
   {
     brand: DeviceBrand.Garmin,
     label: 'Garmin',
-    logoSrc: '/images/device-garmin.svg',
+    logoSrc: '/images/device/garmin.svg',
   },
   {
     brand: DeviceBrand.Suunto,
     label: 'Suunto',
-    logoSrc: '/images/device-suunto.svg',
+    logoSrc: '/images/device/suunto.svg',
   },
   {
     brand: DeviceBrand.Wahoo,
     label: 'Wahoo',
-    logoSrc: '/images/device-wahoo.svg',
+    logoSrc: '/images/device/wahoo.svg',
   },
   {
     brand: DeviceBrand.Polar,
     label: 'Polar',
-    logoSrc: '/images/device-polar.svg',
+    logoSrc: '/images/device/polar.svg',
   },
   {
     brand: DeviceBrand.Coros,
     label: 'Coros',
-    logoSrc: '/images/device-coros.png',
+    logoSrc: '/images/device/coros.png',
   },
   {
     brand: DeviceBrand.Igpsport,
     label: 'iGPSPORT',
-    logoSrc: '/images/device-igpsport.svg',
+    logoSrc: '/images/device/igpsport.svg',
   },
 ];
